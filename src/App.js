d58786e3b58c79850435a0047c05ccc788fc94ce
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2023-01-26 20:33:10 -0300

    add user scene

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import UsersScene from "./scenes/UsersScene";
 function App() {
   // return <UserRegisterScene />;
   return <UsersScene />;
+  // Exercício aula 26/01
+  // Criar a Página de listagem de endereços
+  // Essa página irá utilizar o componente do Header e também irá criar um componente novo
+  // para o card de endereços. Pode seguir o padrão como foi feito a página de usuários.
 }
 
 export default App;
